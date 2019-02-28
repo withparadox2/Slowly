95ef2d7f6c81ca81b9a21948f557f369e3ada9a0
withparadox2 <withparadox2@gmail.com>
2019-02-28 18:10:19 +0800

    Develop

--- app/src/main/java/com/withparadox2/showslowly/ShowActivity.java
+++ app/src/main/java/com/withparadox2/showslowly/ShowActivity.java
@@ -35,7 +35,7 @@ public class ShowActivity extends AppCompatActivity {
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    setContentView(R.layout.activity_show);
     RecyclerView recyclerView = findViewById(R.id.rv_friend);
     mAdapter = new FriendAdapter();
     recyclerView.setAdapter(mAdapter);
---
+++ app/src/main/java/com/withparadox2/showslowly/map/MapActivity.java
@@ -0,0 +1,7 @@
+package com.withparadox2.showslowly.map;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+
+}
