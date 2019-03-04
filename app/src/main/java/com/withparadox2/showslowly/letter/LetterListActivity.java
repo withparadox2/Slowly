37356c0037f3601c49cdb10da2c30b657db09947
withparadox2 <withparadox2@gmail.com>
2019-03-04 22:50:12 +0800

    Load more letters

--- app/src/main/java/com/withparadox2/showslowly/letter/LetterListActivity.java
+++ app/src/main/java/com/withparadox2/showslowly/letter/LetterListActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -19,18 +20,22 @@ import com.withparadox2.showslowly.net.ServiceManager;
 import com.withparadox2.showslowly.net.result.LetterListResult;
 import com.withparadox2.showslowly.token.TokenManager;
 import com.withparadox2.showslowly.util.Util;
+import java.util.ArrayList;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LetterListActivity extends BaseActivity {
+  private static final int FIRST_PAGE = 1;
+
   private SwipeRefreshLayout mRefreshLayout;
-  private List<Letter> mLetterList;
+  private List<Letter> mLetterList = new ArrayList<>();
   private LetterAdapter mAdapter;
   private Friend mFriend;
-  private int mPage = 1;
+  private int mPage = FIRST_PAGE;
   private LoadMoreAdapter mMoreAdapter;
+  private LoadMoreAdapter.Enabled mLoadMoreEnabled;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -43,11 +48,11 @@ public class LetterListActivity extends BaseActivity {
 
     mMoreAdapter = LoadMoreWrapper.with(mAdapter)
         .setFooterView(null) // view or layout resource
-        .setShowNoMoreEnabled(true) // enable show NoMoreView，default false
-        .setLoadMoreEnabled(false)
+        .setShowNoMoreEnabled(false) // enable show NoMoreView，default false
         .setListener(new LoadMoreAdapter.OnLoadMoreListener() {
           @Override
           public void onLoadMore(LoadMoreAdapter.Enabled enabled) {
+            mLoadMoreEnabled = enabled;
             loadLetters(mPage);
           }
         }).into(rv);
@@ -57,14 +62,13 @@ public class LetterListActivity extends BaseActivity {
     mRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
     mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
       @Override public void onRefresh() {
-        loadLetters(1);
+        loadLetters(FIRST_PAGE);
       }
     });
-    loadLetters(1);
+    loadLetters(FIRST_PAGE);
   }
 
   private void loadLetters(final int page) {
-    mRefreshLayout.setRefreshing(true);
     ServiceManager.getSlowlyService()
         .listLetters(mFriend.getId(), TokenManager.getPrefToken(), page)
         .enqueue(
@@ -73,14 +77,21 @@ public class LetterListActivity extends BaseActivity {
               public void onResponse(Call<LetterListResult> call,
                   Response<LetterListResult> response) {
                 mRefreshLayout.setRefreshing(false);
-                if (response != null
-                    && response.body() != null
-                    && response.body().getComments() != null) {
-                  mLetterList.addAll(response.body().getComments().getLetterList());
+                if (response.body() != null && response.body().getComments() != null) {
+                  LetterListResult.Comments comments = response.body().getComments();
+                  if (page == FIRST_PAGE) {
+                    mLetterList.clear();
+                  }
+                  mLetterList.addAll(comments.getLetterList());
                   mAdapter.notifyDataSetChanged();
                   mPage = page + 1;
+
+                  if (mLoadMoreEnabled != null) {
+                    boolean hasMore = !TextUtils.isEmpty(comments.getNextPageUrl());
+                    mMoreAdapter.setShowNoMoreEnabled(!hasMore);
+                    mMoreAdapter.setLoadMoreEnabled(hasMore);
+                  }
                 }
-                mMoreAdapter.setLoadMoreEnabled(false);
               }
 
               @Override public void onFailure(Call<LetterListResult> call, Throwable t) {
