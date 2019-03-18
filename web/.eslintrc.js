6a8b8b9498d6d2fb2f25f14785eae17ccf36ae92
withparadox2 <withparadox2@gmail.com>
2019-03-18 10:30:19 +0800

    web:Eslint for vue

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
   rules: {
     'no-console': 'off',
-  }
+    'vue/no-unused-vars': 'error'
+  },
+  "parserOptions": {
+    "parser": "babel-eslint",
+    "sourceType": "module"
+  },
+  "env": {
+    "es6": true
+  },
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    //'plugin:vue/recommended',
+    "plugin:vue/base"
+  ],
 }
