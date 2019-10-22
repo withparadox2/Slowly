a4a99daee73120153d38ed8bc4b55f6ac0f57684
withparadox2 <withparadox2@gmail.com>
2019-10-22 13:06:32 +0800

    Change content of about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,24 +3,34 @@
        v-show="visible"
        @click="visible = false">
     <div class="content">
-      在夜里
-      在风中
-      在永恒的梦境
-      我，失去了你
+      子夜的灯
+      是一条未穿衣裳的
+      小河
+         
+      你的信像一尾鱼游来
+      读水的温暖
+      读你额上动人的鳞片
+         
+      读江河如读一面镜
+      读镜中你的笑
+      如读泡沫
+          
+          — 子夜读信·洛夫
     </div>
   </div>
 </template>
 <style lang="stylus" scoped>
 .content
   position absolute
-  top 10%
-  bottom 10%
+  top 5%
+  bottom 0%
   right 10%
   left 10%
   color white
   white-space pre
-  line-height 90px
-  font-size 50px
+  line-height 40px
+  font-size 25px
+  overflow-y auto
 </style>
 <script>
 export default {
