506c83d9cf0ec2122317a9fe0439c4a888a6ad05
withparadox2 <withparadox2@gmail.com>
2020-05-08 15:59:02 +1200

    Support removing cached email

--- src/components/EmailInput.vue
+++ src/components/EmailInput.vue
@@ -10,13 +10,20 @@
              :key="email"
              class="item"
              @click="checkEmail(email)">
-          {{email}}
+          <el-row type="flex">
+            <el-col :span="20">{{email}}</el-col>
+            <el-col :span="3" title="删除" class="el-remove-email" @click.native.stop="removeEmail(email)">
+              <i class="el-icon-delete"></i>
+            </el-col>
+          </el-row>
         </div>
       </div>
+      
       <el-input v-bind:value="value"
                 v-on:input="$emit('input', $event)"
                 slot="reference"
                 spellcheck="false"
+                :clearable="true"
                 placeholder="电邮" />
     </el-popover>
   </div>
@@ -27,6 +34,8 @@
   box-sizing border-box
   .item
     cursor pointer
+.el-remove-email
+  text-align right    
 </style>
 <script>
 export default {
@@ -44,10 +53,7 @@ export default {
   },
   watch: {
     value() {
-      this.cachedEmails = this.loadEmails().filter(
-        item => item.indexOf(this.value) >= 0
-      )
-      this.visible = !!(this.value && this.cachedEmails.length > 0)
+      this.setPopupState()
     }
   },
   methods: {
@@ -58,17 +64,29 @@ export default {
 
       const emails = this.loadEmails().filter(item => item != email)
       emails.splice(0, 0, email)
-
-      localStorage.setItem("email-cache-json", JSON.stringify(emails))
+      this.saveEmails(emails)
     },
     loadEmails() {
       return JSON.parse(localStorage.getItem("email-cache-json") || "[]") || []
+    },
+    saveEmails(emails) {
+      localStorage.setItem("email-cache-json", JSON.stringify(emails))
     },
     checkEmail(email) {
       this.$emit("input", email)
       this.$nextTick(() => {
         this.visible = false
       })
+    },
+    removeEmail(email) {
+      this.saveEmails(this.loadEmails().filter(item => item != email))
+      this.setPopupState()
+    },
+    setPopupState() {
+       this.cachedEmails = this.loadEmails().filter(
+        item => item.indexOf(this.value) >= 0
+      )
+      this.visible = !!(this.value && this.cachedEmails.length > 0)
     }
   },
   mounted() {
