029d9277551fc4fe665ac542256953a40bbd8056
withparadox2 <withparadox2@gmail.com>
2020-06-10 21:49:26 +1200

    Disable bugsnap in development mode

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -2,9 +2,11 @@ import Vue from "vue"
 import Bugsnag from "@bugsnag/js"
 import BugsnagPluginVue from "@bugsnag/plugin-vue"
 
-Bugsnag.start({
-  apiKey: "66edf57989161661871fd5c1d096ece7",
-  plugins: [new BugsnagPluginVue()],
-})
+if (process.env.NODE_ENV === "production") {
+  Bugsnag.start({
+    apiKey: "66edf57989161661871fd5c1d096ece7",
+    plugins: [new BugsnagPluginVue()],
+  })
 
-Bugsnag.getPlugin("vue").installVueErrorHandler(Vue)
+  Bugsnag.getPlugin("vue").installVueErrorHandler(Vue)
+}
